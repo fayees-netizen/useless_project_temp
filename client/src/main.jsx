8c4d544cd9b3b3f316083908552083e8617e3dc9
fayees-netizen <fayeesbinfaisal07@gmail.com>
2026-09-13 17:04:28 +0530

    renamedroot

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Root from './Root.jsx';
+import AppShell from './AppShell.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
